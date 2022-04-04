6d927b54b2c31867346a7ade738e517a64b75500
Rango22 <topprogrammer22@gmail.com>
2022-04-05 00:28:05 +0200

    change devnet

--- create-new-token.sh
+++ create-new-token.sh
@@ -1,7 +1,7 @@
 #configuration
-NETWORK_NAME="testnet" ## devnet, testnet, mainnet
-PROXY=https://testnet-gateway.elrond.com
-CHAIN_ID="T"
+NETWORK_NAME="devnet" ## devnet, testnet, mainnet
+PROXY=https://devnet-gateway.elrond.com
+CHAIN_ID="D"
 
 WALLET="wallet/dev-wallet.pem"
 ESDT_ISSUE_ADDRESS=erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u
